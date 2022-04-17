372edfc935d72d40beea5cf417d734ffa8d5fbc2
Lester Covey <me@lestercovey.ml>
2022-04-17 22:33:30 +0300

    Initial Commit

---
+++ Cabbage/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Cabbage
+//
+//  Created by Lesterrry on 17.04.2022.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
