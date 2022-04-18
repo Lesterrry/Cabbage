812edf3115e348996dfb845d0e6b4ea1d915eedf
Lester Covey <me@lestercovey.ml>
2022-04-18 16:28:07 +0300

    App skeleton is done 💀

--- Cabbage/AppDelegate.swift
+++ Cabbage/AppDelegate.swift
@@ -10,12 +10,11 @@ import Cocoa
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
+    func applicationDidFinishLaunching(_ aNotification: Notification) { }
 
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
+    func applicationWillTerminate(_ aNotification: Notification) { }
+	@IBAction func visitGithubMenuItemPressed(_ sender: Any) {
+		NSWorkspace.shared.open(URL(string: "https://github.com/Lesterrry/Cabbage")!)
+	}
+	
 }
--- Cabbage/ViewController.swift
+++ Cabbage/ViewController.swift
@@ -6,13 +6,45 @@
 //
 
 import Cocoa
+import AppKit
+import Quartz
+
+class MyQuickLookItem: NSObject, QLPreviewItem {
+	var previewItemURL: URL!
+}
 
 class ViewController: NSViewController {
-
+	
+	//*********************************************************************
+	// MARK: OUTLETS & ACTIONS
+	//*********************************************************************
+	@IBOutlet weak var chooseFilesButton: NSButton!
+	@IBAction func chooseFilesButtonPressed(_ sender: Any) {
+	}
+	@IBOutlet weak var fileSequenceBackButton: NSButton!
+	@IBAction func fileSequenceBackButtonPressed(_ sender: Any) {
+	}
+	@IBOutlet weak var fileSequenceIndexLabel: NSTextField!
+	@IBOutlet weak var fileSequenceForwardButton: NSButton!
+	@IBAction func fileSequenceForwardButtonPressed(_ sender: Any) {
+	}
+	@IBOutlet weak var fileInfoStackView: NSStackView!
+	@IBOutlet weak var fileInfoNameLabel: NSTextField!
+	@IBOutlet weak var fileInfoStatusLabel: NSTextField!
+	@IBOutlet weak var contentView: NSView!
+	
+	//*********************************************************************
+	// MARK: MAIN FUNCTIONS
+	//*********************************************************************
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+		let quickLookView = QLPreviewView()
+		let quickLookItem = MyQuickLookItem()
+		quickLookItem.previewItemURL = URL(fileURLWithPath: "/Users/ajdarnasibullin/Downloads/DJI_0072.mp4")
+		quickLookView.previewItem = quickLookItem
+		quickLookView.frame = NSRect(x: 0, y: 0, width: contentView.frame.width, height: contentView.frame.height)
+		quickLookView.autoresizingMask = [.height, .width]
+		contentView.addSubview(quickLookView)
     }
 
     override var representedObject: Any? {
